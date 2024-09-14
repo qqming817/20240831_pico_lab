a72baa263a25eb1c5867393fff5b9b1e8eb0a914
derekLab <qqming817@gmail.com>
2024-09-14 11:32:39 +0800

    ver2.2

---
+++ lesson5/lesson5_2.py
@@ -0,0 +1,50 @@
+while True:
+    kg = 0
+    cm = 0
+
+    while True:
+        try:
+            cm = int(input("請輸入身高(公分):"))
+            if cm > 300:
+                raise Exception("超過300公分")
+            break
+        except ValueError:
+            print("輸入格式錯誤")
+            continue
+        except Exception as e:
+            print(f'輸入錯誤{cm}')
+            continue
+
+    while True:
+        try:
+            kg = int(input("請輸入體重(公斤):"))
+            if kg > 200:
+                raise Exception("超過200公斤")
+            break
+        except ValueError:
+            print("輸入格式錯誤")
+            continue
+        except Exception as e:
+            print(f'輸入錯誤{kg}')
+            continue
+
+    print(f'身高= {cm} 公分, 體重= {kg} 公斤')
+    cm = (cm / 100) * (cm / 100)
+    bmi = kg / cm
+    print(f'BMI={bmi}')
+    if bmi >= 35:
+        print("過度肥胖")
+    if bmi >= 27:
+        print("中度肥胖")
+    if bmi >= 24:
+        print("適度肥胖")
+    if bmi >= 18.5:
+        print("正常肥胖")
+    else:
+        print("體重過輕")
+
+    paly_again = input("請問繼續嗎？(Y/N)：")
+    if paly_again == "N":
+        break
+
+print("程式結束")
