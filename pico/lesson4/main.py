2117ae2e332150a3a544d77567054d79a6305d24
derek <qqming817@gmail.com>
2024-09-21 11:46:59 +0800

    ver3.1

--- pico/lesson4/main.py
+++ pico/lesson4/main.py
@@ -1,3 +1,15 @@
 from machine import Pin
+import time
+
 led = Pin("LED", Pin.OUT)
-led.value(1)
+status = False
+
+while True:
+    if status == False:
+        led.on()
+        status = True        
+    else:
+        led.off()
+        status = False
+    print(status)
+    time.sleep_ms(100)
