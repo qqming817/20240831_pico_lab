eed3aaf5819e75a2354bf11e3ffa8919f2f7f1b7
derek <qqming817@gmail.com>
2024-09-21 14:48:36 +0800

    ver3.3

--- pico/lesson4/main2.py
+++ pico/lesson4/main2.py
@@ -1,4 +1,14 @@
 from machine import Timer
 
-tim = Timer(period=1000, mode=Timer.ONE_SHOT, callback=lambda t:print(1))
-tim.init(period=5000, mode=Timer.PERIODIC, callback=lambda t:print(1))
+#tim = Timer(period=1000, mode=Timer.ONE_SHOT, callback=lambda t:print(1))
+#tim.init(period=5000, mode=Timer.PERIODIC, callback=lambda t:print(1))
+
+count = 0
+def mycallback(t:Timer):
+    global count
+    count = count + 1
+    print(f"目前mycallback被執行: {count}次")
+    if count >= 10:
+        t.deinit()
+
+local_timer = Timer(period=1000, mode=Timer.PERIODIC, callback=mycallback)
---
+++ pico/lesson4/main3.py
@@ -0,0 +1,25 @@
+from machine import Timer, Pin
+
+myLed = Pin("LED", Pin.OUT)
+
+green_count = 0
+def green_led_callback(t:Timer):
+    global green_count
+    green_count += 1
+    myLed.toggle() #如果現在是開啟(亮的)。改成關閉；反之亦然
+    print(f"Green_timer被執行{green_count}次")
+    if green_count >= 10:
+        t.deinit()
+
+green_led_timer = Timer(period=1000, mode=Timer.PERIODIC, callback=green_led_callback)
+
+red_count = 0
+def red_led_callback(t:Timer):
+    global red_count
+    red_count += 1
+    myLed.toggle() #如果現在是開啟(亮的)。改成關閉；反之亦然
+    print(f"Red_timer被執行{red_count}次")
+    if red_count >= 10:
+        t.deinit()
+
+red_led_timer = Timer(period=2000, mode=Timer.PERIODIC, callback=red_led_callback)
