679c69c968aa75a1f251c07e9e4460bd25b8013e
derek <qqming817@gmail.com>
2024-09-21 16:20:15 +0800

    ver3.4

--- pico/lesson4/main3.py
+++ pico/lesson4/main3.py
@@ -1,25 +1,27 @@
 from machine import Timer, Pin
 
-myLed = Pin("LED", Pin.OUT)
-
+green_led = Pin("LED", Pin.OUT)
 green_count = 0
 def green_led_callback(t:Timer):
     global green_count
     green_count += 1
-    myLed.toggle() #如果現在是開啟(亮的)。改成關閉；反之亦然
+    green_led.toggle() #如果現在是開啟(亮的)。改成關閉；反之亦然
     print(f"Green_timer被執行{green_count}次")
     if green_count >= 10:
         t.deinit()
+        green_led.off()
 
 green_led_timer = Timer(period=1000, mode=Timer.PERIODIC, callback=green_led_callback)
 
+red_led = Pin(15, Pin.OUT) #看接哪個pin角(PGX)
 red_count = 0
 def red_led_callback(t:Timer):
     global red_count
     red_count += 1
-    myLed.toggle() #如果現在是開啟(亮的)。改成關閉；反之亦然
+    red_led.toggle() #如果現在是開啟(亮的)。改成關閉；反之亦然
     print(f"Red_timer被執行{red_count}次")
     if red_count >= 10:
         t.deinit()
+        red_led.off()
 
 red_led_timer = Timer(period=2000, mode=Timer.PERIODIC, callback=red_led_callback)
