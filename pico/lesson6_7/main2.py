e4feaa36253902a87619d345b5590fa28c2b0b30
derek <qqming817@gmail.com>
2024-09-28 11:50:25 +0800

    ver6.6.1

--- pico/lesson6_7/main2.py
+++ pico/lesson6_7/main2.py
@@ -1,4 +1,4 @@
-from machine import Timer, ADC
+from machine import Timer, ADC, Pin
 
 adc = machine.ADC(4)
 conversion_factor = 3.3/(65535)
@@ -15,7 +15,7 @@ def do_thing(t):
 def do_thing_1(t):
     adc1 = ADC(Pin(26))
     duty = adc1.read_u16()
-    print("可變電阻: {round(duty/65535*100)}")
+    print(f"可變電阻: {round(duty/65535*100)}")
 
 #使用多個Timer可執行多個工作
 Timer(period=2000, mode=Timer.PERIODIC, callback=do_thing)
