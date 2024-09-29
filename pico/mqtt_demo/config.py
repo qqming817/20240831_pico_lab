55750ce384c378d57427b02497461621bc51c8d8
derek <qqming817@gmail.com>
2024-09-29 22:41:14 +0800

    mqtt_demo_0.1

---
+++ pico/mqtt_demo/config.py
@@ -0,0 +1,7 @@
+# 在此填入 Blynk Device Info 的資訊
+BLYNK_TEMPLATE_ID   = "TMPL6W2TH_OyQ"
+BLYNK_TEMPLATE_NAME = "MQTT Demo"
+BLYNK_AUTH_TOKEN    = "cCXOyblyTix4HFq8oPw-bgsl-U9134nB"
+
+# 變更預設 Blynk 伺服器。
+BLYNK_MQTT_BROKER   = "blynk.cloud"
