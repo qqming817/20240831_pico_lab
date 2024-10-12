4ea6700dde9fbba857b08070e53119c986a589b9
林偉銘 <qqming817@gmail.com>
2024-10-12 15:07:03 +0800

    ver8.7 訂閱mqtt 取得msg (with Error)

---
+++ lesson13/computer/subscribe_mqtt.py
@@ -0,0 +1,24 @@
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, reason_code, properites):
+    #連線bloker成功時，只會執行一次
+    client.subscribe("SA-12/#")
+    
+def on_message(client, userdata, msg):
+    print(f"Received messagae '{msg.payload.decode()}' on topic '{msg.topic}'")
+
+def main():
+    client = mqtt.Client(callvack_api_version = mqtt.CallbackAPIVersion.VERSION2)
+    #設定用戶名稱密碼
+    username = "pi"
+    password = "raspberry"
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect("192.168.0.252", 1883, 60)
+    client.loop_forever()
+
+if __name__ == "__main__":
+    main()
+                     
+                    
