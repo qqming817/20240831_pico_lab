2a1622837a90eea150dd2a83261f56c3bbe9e6d3
林偉銘 <qqming817@gmail.com>
2024-10-12 15:12:21 +0800

    ver8.71 訂閱mqtt取得msg (fixed)

--- lesson13/computer/subscribe_mqtt.py
+++ lesson13/computer/subscribe_mqtt.py
@@ -1,14 +1,14 @@
 import paho.mqtt.client as mqtt
 
-def on_connect(client, userdata, reason_code, properites):
+def on_connect(client, userdata, flags, reason_code, properties):
     #連線bloker成功時，只會執行一次
     client.subscribe("SA-12/#")
     
 def on_message(client, userdata, msg):
-    print(f"Received messagae '{msg.payload.decode()}' on topic '{msg.topic}'")
+    print(f"Received message '{msg.payload.decode()}' on topic '{msg.topic}'")
 
 def main():
-    client = mqtt.Client(callvack_api_version = mqtt.CallbackAPIVersion.VERSION2)
+    client = mqtt.Client(callback_api_version = mqtt.CallbackAPIVersion.VERSION2)
     #設定用戶名稱密碼
     username = "pi"
     password = "raspberry"
