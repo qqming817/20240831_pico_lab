f3c586e3b3baad1717fe4713561d73fc47f9d9d5
林偉銘 <qqming817@gmail.com>
2024-10-12 10:21:24 +0800

    ver8.1 with [Errno 104] ECONNRESET

--- lesson13/main.py
+++ lesson13/main.py
@@ -1,40 +1,78 @@
+#! usr/bin/micropython
+
 import tools
 
-from machine import Timer,ADC,Pin,PWM,RTC
+'''
+LED -> GPIO 15
+光敏電阻 -> GPIO 28
+可變電阻 -> GPIO 26
+內建溫度Sensor -> ADC最後1Pin GPIO 28
+'''
+from machine import Timer, ADC, Pin, PWM, RTC
+import binascii
+from umqtt.simple import MQTTClient
+
 def do_thing(t):
     '''
-    處理溫度和光線
+    :param t:Timer的實體
+    負責偵測溫度和光線
     '''
+    
+    conversion_factor = 3.3/(65535)
     reading = adc.read_u16() * conversion_factor
+    
+    # The temperature sensor measures the Vbe voltage of a biased bipolar diode, connected to the fifth ADC channel
+    # Typically, Vbe = 0.706V at 27 degrees C, with a slope of -1.721mV (0.001721) per degree. 
     temperature = 27 - (reading - 0.706)/0.001721
-    print(f'溫度:{temperature}')
-    light_value = adc_light.read_u16()
-    print(f'光線:{light_value}')
+    ligth = adc_light.read_u16()
+    #print(datetime_str)
     
-    
-def do_thing1(t):
+    print(f"溫度:{temperature}")
+    print(f"光線:{ligth}")
+
+#可變電阻
+def do_thing_1(t):
     '''
-    處理可變電阻
-    '''
-    adc1 = ADC(Pin(26))
-    duty = adc1.read_u16()
-    pwm.duty_u16(duty)    
-    print(f'可變電阻:{round(duty/65535*10)}')
+    :param t:Timer的實體
+    負責偵測可變電阻和改變LED的亮度
+    '''    
+    duty = adc1.read_u16()    
+    pwm.duty_u16(duty) #ADC可變電阻電壓輸出給PWM控制LED登亮度
     
+    light_lv = round(duty/65535*100)
+    print(f"可變電阻: {light_lv}")
+    #mqtt.publish('SA-12/亮度', f'{light_lv}')
+    mqtt.publish('SA-12/亮度', f'{light_lv}')
+
+def do_reconnect(t):
+    tools.reconnect()
+
 def main():
-    t1 = Timer(period=1000, mode=Timer.PERIODIC, callback=do_thing)
-    t2 = Timer(period=500, mode=Timer.PERIODIC, callback=do_thing1)
+    pass
     
 if __name__ == "__main__":
-    #pico_連結電腦時的寫法,要用connect
+    #tools.connect() #連線到Wifi
+    adc = machine.ADC(4) #內建溫度 
+    adc1 = ADC(Pin(26)) #可變電阻
+    adc_light = ADC(Pin(28)) #PWM LED
+    pwm = PWM(Pin(15), freq=65535) #freq要給
+    
+    #連線到Internet
     try:
         tools.connect()
     except RuntimeError as e:
-        print(f"{{e}")
+        print(e)
+    except Exception:
+        print('不明的錯誤')
     else:
-        #sensor setup
-        adc = ADC(4) #內建溫度感測器
-        adc_light = ADC(Pin(28)) #光線感測器
-        pwm = PWM(Pin(15),freq=50) #可變電阻
-        conversion_factor = 3.3 / (65535) #電壓轉換率    
-        main()
+        #MQTT
+        SERVER = "192.168.0.252"
+        CLIENT_ID = binascii.hexlify(machine.unique_id())
+        mqtt = MQTTClient(CLIENT_ID, SERVER, user='pi', password='raspberry')
+        mqtt.connect()
+    
+        #使用多個Timer可執行多個工作
+        Timer(period=2000, mode=Timer.PERIODIC, callback=do_thing)
+        Timer(period=1000, mode=Timer.PERIODIC, callback=do_thing_1)
+
+    main()
