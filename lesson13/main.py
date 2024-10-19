d5390a8ff26f8e04bb375f7b573fdaaad9cc3b03
林偉銘 <qqming817@gmail.com>
2024-10-19 11:13:56 +0800

    v9.3 modify to round()

--- lesson13/main.py
+++ lesson13/main.py
@@ -23,8 +23,8 @@ def do_thing(t):
     
     # The temperature sensor measures the Vbe voltage of a biased bipolar diode, connected to the fifth ADC channel
     # Typically, Vbe = 0.706V at 27 degrees C, with a slope of -1.721mV (0.001721) per degree. 
-    temperature = 27 - (reading - 0.706)/0.001721
-    print(f"溫度:{round(temperature,2)}")    
+    temperature = round(27 - (reading - 0.706)/0.001721, 2)
+    print(f"溫度:{temperature}")    
     mqtt.publish('SA-12/TEMPERATURE', f'{temperature}')
 
     light_lv = adc_light.read_u16()
