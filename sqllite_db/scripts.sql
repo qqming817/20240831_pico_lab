a209d76dbed2a41ea2e258d995c37722ac9ec143
林偉銘 <qqming817@gmail.com>
2024-10-26 14:56:19 +0800

    v10.3 Sqlite insert date from python

---
+++ sqllite_db/scripts.sql
@@ -0,0 +1,7 @@
+SELECT * FROM chicken_accmodation;
+
+INSERT INTO chicken_accmodation (record_date, equipment, value) VALUES (
+'24-10-12 16:17:38', 'SA-12/LED_LEVEL', 37.8)
+
+INSERT INTO chicken_accmodation (record_date, equipment, value) VALUES (
+datetime('now'), 'SA-12/LED_LEVEL', 32.2)
